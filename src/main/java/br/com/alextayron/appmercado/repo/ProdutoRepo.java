66ce98e0ae92105ef6a8e1e5e6019436f29e3806
Alex Tayron <57293582+AlexTayron@users.noreply.github.com>
2022-10-04 18:24:43 -0300

    mudando pastas

--- src/main/java/br/com/alextayron/appmercado/repo/ProdutoRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.alextayron.appmercado.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.alextayron.appmercado.model.Produto;
-
-public interface ProdutoRepo extends CrudRepository<Produto, Integer>{
-	
-}
